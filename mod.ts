4db7e419d1fa0a9442903df0075aff488b9d6bb5
tam.vu <vuthanhtam1506@gmail.com>
2022-12-08 17:19:08 +0700

    [Fix] Change order of how code is return, since Deno is not parsing it properly

--- mod.ts
+++ mod.ts
@@ -1,7 +1,4 @@
-const composeMRight =
-  (method) =>
-  (...ms) =>
-    ms.reduceRight((f, g) => (x) => g(x)[method](f));
+const composeMRight = (method) => (...ms) => ms.reduceRight((f, g) => (x) => g(x)[method](f));
 //This one is used to inject your own logic onto the current execution
 //Result of the previous function is the args of the current function
 const composePromises = composeMRight("then");
